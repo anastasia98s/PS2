7120f272d198ae4bec23d6da985e1d3b9749543f
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2024-11-18 02:11:30 +0100

    update try catch

--- intents/wetter_intent.py
+++ intents/wetter_intent.py
@@ -12,32 +12,39 @@ class WetterIntent(Datenkonverter):
         if not i_ort:
             i_ort = config.DEFAULT_ORT
 
-        if i_datum:
-            if i_zeit:
-                datezeit, errortyp = super().date_zeit_konverter(i_datum, i_zeit) # Die API kann derzeit an bestimmten Tagen keine Abfragen durchführen
-                if not datezeit:
-                    return None, errortyp
-                bedingung_url = f"http://wttr.in/{i_ort}?format=%C&lang=de"
-                temperatur_url = f"http://wttr.in/{i_ort}?format=%t"
-            else:
-                datum, errortyp = super().date_konverter(i_datum) # API Die API kann derzeit an bestimmten Tagen keine Abfragen durchführen
-                if not datum:
-                    return None, errortyp
+        try:
+            if i_datum:
+                if i_zeit:
+                    datezeit, errortyp = super().date_zeit_konverter(i_datum, i_zeit) # Die API kann derzeit an bestimmten Tagen keine Abfragen durchführen
+                    if not datezeit:
+                        return None, errortyp
+                    bedingung_url = f"http://wttr.in/{i_ort}?format=%C&lang=de"
+                    temperatur_url = f"http://wttr.in/{i_ort}?format=%t"
                 else:
-                    datum = datum.strftime("%Y-%m-%d")
-                    
+                    datum, errortyp = super().date_konverter(i_datum) # API Die API kann derzeit an bestimmten Tagen keine Abfragen durchführen
+                    if not datum:
+                        return None, errortyp
+                    else:
+                        datum = datum.strftime("%Y-%m-%d")
+                        
+                    bedingung_url = f"http://wttr.in/{i_ort}?format=%C&lang=de"
+                    temperatur_url = f"http://wttr.in/{i_ort}?format=%t"
+            else:
                 bedingung_url = f"http://wttr.in/{i_ort}?format=%C&lang=de"
                 temperatur_url = f"http://wttr.in/{i_ort}?format=%t"
-        else:
-            bedingung_url = f"http://wttr.in/{i_ort}?format=%C&lang=de"
-            temperatur_url = f"http://wttr.in/{i_ort}?format=%t"
 
-        bedingung_response = requests.get(bedingung_url)
-        temperatur_response = requests.get(temperatur_url)
-        if bedingung_response.status_code == 200 and temperatur_response.status_code == 200:
-            bedingung = bedingung_response.text.strip()
-            temperatur = temperatur_response.text.strip()
-            temperatur = ''.join(filter(str.isdigit, temperatur))
-            return f"in {i_ort} {i_datum} {i_zeit} {bedingung} {temperatur} Grad", None
-        else:
-            return None, config.ERROR_VARIABLE_ORT
+            bedingung_response = requests.get(bedingung_url)
+            temperatur_response = requests.get(temperatur_url)
+            if bedingung_response.status_code == 200 and temperatur_response.status_code == 200:
+                bedingung = bedingung_response.text.strip()
+                temperatur = temperatur_response.text.strip()
+                temperatur = ''.join(filter(str.isdigit, temperatur))
+                return f"in {i_ort} {i_datum} {i_zeit} {bedingung} {temperatur} Grad", None
+            else:
+                return None, config.ERROR_VARIABLE_ORT
+        except requests.Timeout:
+            return "Fehler: Zeitüberschreitung bei der Verbindung zur Wetter-API", None
+        except requests.RequestException as e:
+            return None, f"Bei der Wetterabfrage ist ein Fehler aufgetreten"
+        except Exception as e:
+            return None, f"Unerwarteter Fehler bei der Wetterabfrage"
