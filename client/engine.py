44dcfce27434227e5aa65cdf8cb216433d39b1d7
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2024-12-10 16:20:02 +0100

    update

--- client/engine.py
+++ client/engine.py
@@ -33,7 +33,7 @@ class Engine:
                 if any(word in antwort_text.lower().split() for word in ["ja", "genau"]):
                     return pred_id, None
             
-            if isinstance(name_label, list) and len(name_label[0]) > 1:
+            if isinstance(name_label, list) and len(name_label) > 0 and len(name_label[0]) > 1:
                 antwort_text = self.speech_to_text.dialog(0.5, "Haben Sie bereits ein Konto?")
                 if any(word in antwort_text.lower().split() for word in ["ja", "genau"]):
                     for label in name_label[0]:
